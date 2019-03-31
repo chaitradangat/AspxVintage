6792f0a8219aff26948ec84acbd56e4a64d18b3a
Chaitra <chaitradangat084@gmail.com>
2019-03-31 23:34:49 +0530

    wcf service

---
+++ AspxVintage/WCFServices/WCFService.svc.cs
@@ -0,0 +1,18 @@
+﻿namespace AspxVintage.WCFServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WCFService.svc or WCFService.svc.cs at the Solution Explorer and start debugging.
+    public class WCFService : IWCFService
+    {
+        public void DoWork()
+        {
+
+        }
+
+        public string GetData()
+        {
+            return "Hello WCF!";
+        }
+
+    }
+}
